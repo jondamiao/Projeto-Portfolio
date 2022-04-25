6cf60d9753a9b563c43fa7d0cbf29aed7230879f
jonatas.paz <jonatas.paz@imusica.com.br>
2022-04-25 14:59:51 -0300

    Subindo projeto finalizado

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="component">
+    <HomeTemplate />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { HomeTemplate } from '@/components/Templates'
+
+export default defineComponent({
+  components: { HomeTemplate }
+})
+</script>
+
+<style lang="scss" scoped>
+.component {
+  margin: 5rem;
+}
+</style>
---
+++ src/components/organisms/Services.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="services">
+    <Service
+      title="Front End"
+      description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga soluta obcaecati ullam provident maiores laboriosam quis? Atque asperiores saepe eligendi veniam laboriosam excepturi, impedit voluptatibus tempora sequi, debitis deserunt magni?"
+    />
+
+    <Service
+      title="Back End"
+      description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga soluta obcaecati ullam provident maiores laboriosam quis? Atque asperiores saepe eligendi veniam laboriosam excepturi, impedit voluptatibus tempora sequi, debitis deserunt magni?"
+    />
+
+    <Service
+      title="Manutençao"
+      description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga soluta obcaecati ullam provident maiores laboriosam quis? Atque asperiores saepe eligendi veniam laboriosam excepturi, impedit voluptatibus tempora sequi, debitis deserunt magni?"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Service } from '@/components/molecules'
+
+export default defineComponent({
+  components: { Service }
+})
+</script>
+
+<style lang="scss" scoped>
+.services {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5rem;
+}
+</style>
